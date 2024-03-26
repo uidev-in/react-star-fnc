7836a1dffeb39baca21b1ced050ec55c7400b6c8
akbaruddin <akbaruddin@live.in>
2024-03-26 22:52:24 +0530

    feat: init v0.0.0

---
+++ src/stories/ReactStars.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+
+import { ReactStars } from '..';
+
+export default {
+  title: 'ReactStars',
+  component: ReactStars,
+  argTypes: {},
+} as Meta<typeof ReactStars>;
+
+const Template: StoryFn<typeof ReactStars> = (args) => <ReactStars {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  size: 30,
+  value: 2.5,
+  edit: false,
+};
+
+export const CustomIcon = Template.bind({});
+
+CustomIcon.args = {
+  size: 50,
+  count: 10,
+  char: '',
+  unSelectedColor: '#ff9900',
+  selectedColor: '#6599ff',
+  onChange: (newValue: any) => {
+    console.log(`Example 2: new value is ${newValue}`);
+  },
+};
+
+export const WithoutHalf = Template.bind({});
+
+WithoutHalf.args = {
+  size: 40,
+  count: 7,
+  half: false,
+  value: 4,
+  onChange: (newValue: any) => {
+    console.log(`Example 3: new value is ${newValue}`);
+  },
+};
